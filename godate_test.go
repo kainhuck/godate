169a51fd96e7fcc65c3f6782e2ea9bcb700b1ae5
kainhuck <35602940+kainhuck@users.noreply.github.com>
2023-07-07 11:04:26 +0800

    add a demo

--- godate_test.go
+++ godate_test.go
@@ -1,7 +1,6 @@
 package godate
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
@@ -1386,15 +1385,5 @@ func Test_isLeapYear(t *testing.T) {
 				t.Errorf("isLeapYear() = %v, want %v", got, tt.want)
 			}
 		})
-	}
-}
-
-func TestAddDaysOfYear2(t *testing.T) {
-	start, _ := NewDate(1970, 1, 1)
-	end := Today()
-
-	for start.EarlyThan(end) {
-		fmt.Println(start.String())
-		start.Increase()
 	}
 }
